84520efda7c41771df7ba9656f58925a1e77c798
vova <alo>
2022-12-05 11:02:20 +0500

    Добавьте файлы проекта.

---
+++ Romania/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using RomaniaLibrary;
+
+namespace Romania
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int gender,resident;
+        public MainWindow()
+        {
+            InitializeComponent();
+            GenerateList();
+        }
+
+        private void GenerateList()
+        {
+            string[] readText = File.ReadAllLines(@"C: \Users\311 - 20(student)\source\repos\Romania\Romania\bin\Debug");
+            List<string> list = new List<string> { };
+            foreach (var item in readText)
+            {
+              string[] result= item.Split();
+                list.Add(result[2]);
+            }
+        }
+
+        private void GenerateCNP_Click(object sender, RoutedEventArgs e)
+        {
+            CodeClass obj = new CodeClass();
+            if (Male.IsChecked==true)
+            {
+                gender = 1;
+            }
+            else
+            {
+                gender = 2;
+            }
+            if (RadioButtonYes.IsChecked==true)
+            {
+                resident = 1;
+            }
+            else
+            {
+                resident = 2;
+            }
+             obj.GenerateCode(LastName.Text, gender,DateOfBirth.SelectedDate,)
+
+        }
+    }
+}
---
+++ RomaniaLibrary/CodeClass.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace RomaniaLibrary
+{
+    public class CodeClass
+    {
+        
+        public string GenerateCode(string lastName, int gender, DateTime dateBirdth, string resident/*, string city*/)
+        {
+            string number = "";
+            if (dateBirdth.Year>=1900&& dateBirdth.Year<=1949)
+            {
+                number +="1";
+            }
+            if (dateBirdth.Year >= 1950 && dateBirdth.Year <= 1999)
+            {
+                number += "2";
+            }
+            if (dateBirdth.Year >= 1800 && dateBirdth.Year <= 1849)
+            {
+                number += "3";
+            }
+            if (dateBirdth.Year >= 1850 && dateBirdth.Year <= 1899)
+            {
+                number += "4";
+            }
+            if (dateBirdth.Year >= 2000 && dateBirdth.Year <= 2049)
+            {
+                number += "5";
+            }
+            if (dateBirdth.Year >= 2050&& dateBirdth.Year <= 2099)
+            {
+                number += "6";
+            }
+            string year = Convert.ToString(dateBirdth.Year).Substring(2,2);
+            string month = Convert.ToString(dateBirdth.Month).Substring(0,2);
+            string day = Convert.ToString(dateBirdth.Day);
+            number += year;
+            number += year;
+            number += month;
+            number += month;
+            number += day;
+            number += day;
+            //
+            if (gender=="1")
+            {
+                number +=1;
+            }
+            if (gender == "2")
+            {
+                number += 2;
+            }
+
+
+            return number;
+
+        }
+    }
+}
